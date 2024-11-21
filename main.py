4b22c4b24fe52fad1111ce30d85cb3fa8571f758
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-11-21 21:12:28 +0100

    added parsing functionality

--- main.py
+++ main.py
@@ -19,21 +19,38 @@ from train_ddpm import train_ddpm_epoch
 from sample_ddpm import sample_ddpm
 from dataloader import get_dataloader
 
-# Argument parser
-# TODO Implement if script is to be run on HPC 
+## Argument parser
+parser = argparse.ArgumentParser(description='Train (and sample from) DDPM framework.')
+
+## Add arguments
+parser.add_argument('--T', type=int, default=1000, help='Total timesteps.')
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size.')
+parser.add_argument('--num_epochs', type=int, default=5, help='Number of epochs.')
+parser.add_argument('--lr', type=float, default=2e-4, help='Learning rate.')
+parser.add_argument('--dataset', type=str, default='MNIST', help='Dataset to use (MNIST or CIFAR10).')
+parser.add_argument('--save_model', type=bool, default=True, help='Save model after training.')
+parser.add_argument('--wandb', default="online", type=str, choices=["online", "disabled"] , help="whether to track with weights and biases or not")
 
 def main():
-    # Initialize Weights and Biases
-    wandb.init(project='ddpm', entity='dl_ddpm')
+    # Parse arguments
+    args = parser.parse_args()
 
-    T = 1000  # Total timesteps
-    batch_size = 64  # Batch size
-    num_epochs = 5  # Number of epochs
-    lr = 2e-4  # Learning rate
-    dataset = "MNIST"  # Dataset to use ('mnist' or 'cifar10')
-    save_model = True  # Save model after training
+    # Unpack arguments
+    T = args.T
+    batch_size = args.batch_size
+    num_epochs = args.num_epochs
+    lr = args.lr
+    dataset = args.dataset
+    save_model = args.save_model
 
     save_dir = "./saved_models"  # Directory to save the trained model
+
+    # Set mode for Weights and Biases
+    mode_for_wandb = args.wandb
+    run_name = f"ddpm_{dataset}_T_{T}_bs_{batch_size}_lr_{lr}"
+
+    # Initialize Weights and Biases
+    wandb.init(project='ddpm', entity='dl_ddpm', mode=mode_for_wandb, name=run_name)
 
     device = torch.device("cuda" if torch.cuda.is_available() else 
                       "mps" if torch.backends.mps.is_available() else 
@@ -76,4 +93,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print("Training completed successfully!")
+    
